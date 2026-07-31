e023d4b1ac7ac33fd60cb47cd9ab3173a4d6131a
killjapskin <killjapskin@gmail.com>
2026-07-31 22:47:01 +0300

    Test main script loading

--- plugin.js
+++ plugin.js
@@ -2,6 +2,10 @@
     'use strict';
 
     Lampa.Utils.putScriptAsync([
-        'https://killjapskin.github.io/lampa-series-notifier/SeriesNotify/SeriesNotify.js'
-    ], function () {});
+        'https://killjapskin.github.io/lampa-series-notifier/SeriesNotify/SeriesNotify.js?v=2'
+    ], function () {
+        if (window.Lampa && Lampa.Noty) {
+            Lampa.Noty.show('Series Notify: основной файл загружен');
+        }
+    });
 })();
