fa956fff7f6a3a3f4dd8b3a1060ddd1a80e7fe2a
killjapskin <killjapskin@gmail.com>
2026-07-31 22:48:51 +0300

    Restore main loader

--- plugin.js
+++ plugin.js
@@ -3,9 +3,5 @@
 
     Lampa.Utils.putScriptAsync([
         'https://killjapskin.github.io/lampa-series-notifier/SeriesNotify/SeriesNotify.js?v=2'
-    ], function () {
-        if (window.Lampa && Lampa.Noty) {
-            Lampa.Noty.show('Series Notify: основной файл загружен');
-        }
-    });
+    ], function () {});
 })();
